ca7151728069d4c45a91431859f9bc86a6c59771
shaysudaei <shysud5@gmail.com>
2025-01-18 20:16:51 +0200

    add base mui

---
+++ src/components/views/ItemFormView/ItemFormView.css.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ItemFormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+`;
+
+export const ItemFormInputs = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`;
+
--- src/components/views/ItemFormView/ItemFormView.tsx
+++ src/components/views/ItemFormView/ItemFormView.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import { Dropdown } from '@mui/base/Dropdown';
+import { MenuButton } from '@mui/base/MenuButton';
+import { Menu } from '@mui/base/Menu';
+import { MenuItem } from '@mui/base/MenuItem';
+import './ItemFormView.css.js';
+
+
 
 const ItemFormView = ({ categories, addItem }) => {
+
     return (
         <div>
             <h2>Item Form</h2>
             <div>
-                <div>Categories:</div>
-                <ul>
+            <div> name: <input type='text'/> </div>
+            <div> cost: <input type='text'/> </div>
+            <Dropdown>
+                <MenuButton>Categories</MenuButton>
+                <Menu slots={{ listbox: 'div' }}>
                     {categories.map((category, index) => (
-                        <li key={category + index}>{category}</li>
+                        <MenuItem key={index}>{category}</MenuItem>
                     ))}
-                </ul>
+                </Menu>
+            </Dropdown>
             </div>
+            <div> description: <input type='text'/> </div>
             <Button variant="contained" color="primary" onClick={addItem}>
                 Add Item
             </Button>
