52f65b94b7c2c1cf09832b43c4717ee295c07980
shani kupiec <shani.kupiec@gmail.com>
2025-01-18 20:08:00 +0200

    save changes

--- src/components/views/ItemFormView/ItemFormView.tsx
+++ src/components/views/ItemFormView/ItemFormView.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { Button } from '@mui/material';
 
-const ItemFormView = ({ categories, addItem }) => {
+const ItemFormView = ({ item, categories, saveItem }) => {
+
     return (
         <div>
             <h2>Item Form</h2>
-            <div>
-                <div>Categories:</div>
-                <ul>
-                    {categories.map((category, index) => (
-                        <li key={category + index}>{category}</li>
-                    ))}
-                </ul>
-            </div>
-            <Button variant="contained" color="primary" onClick={addItem}>
+            <Button variant="contained" color="primary" onClick={saveItem}>
                 Add Item
             </Button>
         </div>
