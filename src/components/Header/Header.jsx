d8c7d9a02a591fc0e2f68d400da8063e2e63da3d
shani kupiec <shani.kupiec@gmail.com>
2025-01-22 12:38:53 +0200

    add index db

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,12 @@ import { HeaderContainer, NavButton } from './Header.css';
 const Header = ({ views, currentView, onViewChange }) => {
     return (
         <HeaderContainer>
+            {/* Map through the views object, creating a NavButton for each entry */}
             {Object.entries(views).map(([key, { label }]) => (
                 <NavButton
                     key={key}
-                    isactive={currentView === key}
-                    onClick={() => onViewChange(key)}
+                    isactive={currentView === key} // Highlight the active view
+                    onClick={() => onViewChange(key)} 
                 >
                     {label}
                 </NavButton>
